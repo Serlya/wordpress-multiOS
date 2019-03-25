c342ff3b87e2490c178b0dcdb2d18caeb263b425
SuniL <45892241+Serlya@users.noreply.github.com>
2019-03-25 11:46:05 +0530

    Update ubuntu.php

--- ubuntu.php
+++ ubuntu.php
@@ -3,6 +3,6 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello ZippyOPS Consulting Services</p>'; ?>
+ <?php echo '<p>Hello ZippyOPS Consulting Services DevOps</p>'; ?>
  </body>
 </html>
